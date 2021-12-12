4c76c47e29b5b6529c1244822bee6a99c03fb713
Andreas Leathley <andreas.leathley@panaxis.ch>
2021-12-12 12:37:22 +0100

    Shorten OriginException property definitions

--- src/OriginException.php
+++ src/OriginException.php
@@ -4,41 +4,20 @@ namespace Squirrel\Debug;
 
 class OriginException extends \Exception
 {
-    /**
-     * @var string Original call which lead to the exception
-     */
-    private string $originCall = '';
-
-    /**
-     * @var string File in which the problem originated
-     */
-    private string $originFile = '';
-
-    /**
-     * @var int Line on which the problem originated
-     */
-    private int $originLine = 0;
-
-    /**
-     * @var string File in which the exception was thrown
-     */
+    /** @var string File in which the exception was thrown */
     private string $exceptionFile = '';
 
-    /**
-     * @var int Line on which the exception was thrown
-     */
+    /** @var int Line on which the exception was thrown */
     private int $exceptionLine = 0;
 
-    /**
-     * @param string $originCall Original call which lead to the exception
-     * @param string $originFile File in which the problem originated
-     * @param int $originLine Line on which the problem originated
-     */
     public function __construct(
-        string $originCall,
-        string $originFile,
-        int $originLine,
-        string $message = "",
+        /** @var string Original call which lead to the exception */
+        private string $originCall,
+        /** @var string File in which the problem originated */
+        private string $originFile,
+        /** @var int Line on which the problem originated */
+        private int $originLine,
+        string $message = '',
         int $code = 0,
         ?\Throwable $previous = null,
     ) {
@@ -46,10 +25,6 @@ class OriginException extends \Exception
 
         $this->exceptionFile = $this->getFile();
         $this->exceptionLine = $this->getLine();
-
-        $this->originCall = $originCall;
-        $this->originFile = $originFile;
-        $this->originLine = $originLine;
 
         // Replace the exception file and line to point to the more accurate origin
         $this->file = $originFile;
